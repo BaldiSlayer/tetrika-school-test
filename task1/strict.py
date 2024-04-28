9e6256745f671e49f258a64da4e73155d2bd7271
BaldiSlayer <lisov.a2005@yandex.ru>
2024-04-28 18:34:28 +0300

    added kwargs types checking for strict decorator

--- task1/strict.py
+++ task1/strict.py
@@ -3,10 +3,14 @@ import inspect
 
 def strict(func):
     def wrapper(*args, **kwargs):
-        signature_arguments = inspect.signature(func).parameters.values()
-        for arg_name, arg_value in zip(signature_arguments, args):
+        signature_arguments = inspect.signature(func).parameters
+        for arg_name, arg_value in zip(signature_arguments.values(), args):
             if not isinstance(arg_value, arg_name.annotation):
                 raise TypeError(f'Argument {arg_name.name} must be of type {arg_name.annotation}')
+
+        for arg_name, arg_value in kwargs.items():
+            if not isinstance(arg_value, signature_arguments[arg_name].annotation):
+                raise TypeError(f'Argument {arg_name} must be of type {signature_arguments[arg_name].annotation}')
 
         return func(*args, **kwargs)
 
@@ -17,6 +21,3 @@ def strict(func):
 def sum_two(a: int, b: int) -> int:
     return a + b
 
-
-# print(sum_two(1, 2))  # >>> 3
-# print(sum_two(1.0, 2.4))  # >>> TypeError
