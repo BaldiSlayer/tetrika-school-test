14ad2b169b1bebd58567ccc52efb03a2b8f402d8
BaldiSlayer <lisov.a2005@yandex.ru>
2024-04-29 23:02:51 +0300

    added comments for task1

--- task1/strict.py
+++ task1/strict.py
@@ -3,11 +3,15 @@ import inspect
 
 def strict(func):
     def wrapper(*args, **kwargs):
+        # getting parameters from the function signature
         signature_arguments = inspect.signature(func).parameters
+
+        # checking args types matching
         for arg_name, arg_value in zip(signature_arguments.values(), args):
             if not isinstance(arg_value, arg_name.annotation):
                 raise TypeError(f'Argument {arg_name.name} must be of type {arg_name.annotation}')
 
+        # checking kwargs types matching
         for arg_name, arg_value in kwargs.items():
             if not isinstance(arg_value, signature_arguments[arg_name].annotation):
                 raise TypeError(f'Argument {arg_name} must be of type {signature_arguments[arg_name].annotation}')
